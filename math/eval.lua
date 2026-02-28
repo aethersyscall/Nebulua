7ae7fff8a2623f40c01a031e195c15a54dcc2103
AetherSyscall <aethersyscall@proton.me>
2026-03-01 00:14:31 +0530

    Refactor: Eval supports Luajit now

--- math/eval.lua
+++ math/eval.lua
@@ -4,12 +4,22 @@ local function eval(expression)
   end
 
   local safe_env = {}
-
   for k, v in pairs(math) do
     safe_env[k] = v
   end
 
-  local chunk, err = load("return " .. expression, "nebulua_math", "t", safe_env)
+  local code = "return " .. expression
+  local chunk, err
+
+  if setfenv then
+    local compiler = loadstring or load
+    chunk, err = compiler(code, "nebulua_math")
+    if chunk then
+      setfenv(chunk, safe_env)
+    end
+  else
+    chunk, err = load(code, "nebulua_math", "t", safe_env)
+  end
 
   if not chunk then
     return nil, "Syntax Error: " .. (err or "Unknown")
